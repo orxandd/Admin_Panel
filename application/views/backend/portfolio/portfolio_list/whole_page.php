e8cf21a4110983aa6796309dee028d1fbcd433fe
JavidLalayev <cavidleleyev9a12014@gmail.com>
2019-08-19 11:03:11 +0400

    dinamik filter hazirlandi

--- application/views/backend/portfolio/portfolio_list/whole_page.php
+++ application/views/backend/portfolio/portfolio_list/whole_page.php
@@ -9,9 +9,31 @@
             <h1 class="c_title">Portfellər</h1>
             <div class="grid simple ">
                 <div class="row" style="margin-top: 50px">
-                    <div class="grid-body no-border c_grid_padding c_resfresh_portfolio_category">
 
-                        <?php $this->load->view("$this->parent_folder/$this->sub_folder/portfolio_list/portfolio_list_render_page/portfolio_table");?>
+
+
+                    <div class="grid-body no-border c_grid_padding">
+
+                        <select name="category_filter" id="c_filter" data-url = <?php echo base_url("secure_admin_panel_portfolio_filter")?> >
+
+                            <option value="all">Hamısı</option>
+                            <?php foreach ($portfolio_categories as $portfolio_category) { ?>
+
+                                <option value="<?php echo $portfolio_category["name"]?>"><?php echo $portfolio_category["name"]?></option>
+
+                            <?php }?>
+                        </select>
+                        <i class="fa fa-spinner fa-spin" id="c_spinner" style="margin-left: 10px; font-size: 20px"></i>
+
+                        <a href="<?php echo base_url("secure_admin_panel_portfolio_add")?>" class="btn btn-success c_right">
+                            <i class="fa fa-plus"></i>
+                            <span class="c_add_text">Əlavə Et</span>
+                        </a>
+
+
+                        <div class="c_resfresh_portfolio_category">
+                            <?php $this->load->view("$this->parent_folder/$this->sub_folder/portfolio_list/portfolio_list_render_page/portfolio_table");?>
+                        </div>
 
                     </div>
                 </div>
